516f814d75cb55a24f683e0d6ae3e7a3a223625a
DESKTOP-R22FR0H\VuNHT <VuNHT@fsoft.com.vn>
2019-05-28 13:00:19 +0700

    Fix1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,8 @@ import { ToastrService } from 'ngx-toastr';
 export class AppComponent implements OnInit {
   private renderer2: Renderer2;
   public drawImage: any;
+
+  public listClass = [];
   public menuList1 = [
 
   ];
@@ -24,21 +26,54 @@ export class AppComponent implements OnInit {
   }
   ngOnInit() {
 
+    // setTimeout(() => {
+    //   this.initDraw();
+    //   this.draw();
+    // }, 1000);
     setTimeout(() => {
-      this.initDraw();
-      this.draw();
+      this.toastr.info('Click hình để thêm', '', { timeOut: 5000 });
     }, 1000);
-    setTimeout(() => { this.toastr.info('Click add', '', { timeOut : 5000 });
-  }, 2000);
   }
 
+  // public dropImage($event) {
+  //   const that = this;
+  //   const classTag = $event.id;
+  //   this.listClass.push(classTag);
+  //   if (this.listClass.length > 1) {
+  //     setTimeout(() => {
+  //       this.initDraw();
+  //       this.draw();
+  //     }, 1000);
+  //   }
+  // }
+
   public drop($event) {
-        this.menuList1.unshift($event);
+    this.menuList1.unshift($event);
+    if (this.menuList1.length < 2) {
+      this.toastr.info('Di chuyển hình !!', '', { timeOut: 5000 });
+    }
+    if (this.menuList1.length > 1) {
+      this.menuList1.forEach(element => {
+        this.listClass.push(element.id);
+      });
+
+      // setTimeout(() => {
+      //   this.initDraw();
+      //   this.draw();
+      // }, 1000);
+
+    }
   }
 
   public draw() {
-    const divA: any = document.querySelector('#a');
-    const divB: any = document.querySelector('#b');
+    // const divA: any = document.querySelector('#a');
+    // const divB: any = document.querySelector('#b');
+    console.log({
+      0: '#' + this.listClass[0],
+      1: '#' + this.listClass[1]
+    });
+    const divA: any = document.querySelector('#' + this.listClass[0]);
+    const divB: any = document.querySelector('#' + this.listClass[1]);
     const arrowLeft = document.querySelector('#arrowLeft');
     const arrowRight = document.querySelector('#arrowRight');
     console.log({
@@ -48,30 +83,30 @@ export class AppComponent implements OnInit {
       arrowRight: arrowRight
     });
 
-    // const posnALeft = {
-    //   x: divA.offsetLeft - 8,
-    //   y: divA.offsetTop + 8
-    // };
-    // const posnARight = {
-    //   x: divA.offsetLeft + divA.offsetWidth + 8,
-    //   y: divA.offsetTop + divA.offsetHeight / 2
-    // };
-    // const posnBLeft = {
-    //   x: divB.offsetLeft - 8,
-    //   y: divB.offsetTop - 8
-    // };
-    // const posnBRight = {
-    //   x: divB.offsetLeft + divB.offsetWidth + 8,
-    //   y: divB.offsetTop + divA.offsetHeight / 2
-    // };
-    // const dStrLeft =
-    //   'M' +
-    //   (posnALeft.x + 55) + ',' + (posnALeft.y) + ' ' +
-    //   'C' +
-    //   (posnALeft.x + 55) + ',' + (posnALeft.y) + ' ' +
-    //   (posnBLeft.x + 55) + ',' + (posnBLeft.y) + ' ' +
-    //   (posnBLeft.x + 55) + ',' + (posnBLeft.y);
-    // arrowLeft.setAttribute('d', dStrLeft);
+    const posnALeft = {
+      x: divA.offsetLeft - 8,
+      y: divA.offsetTop + 8
+    };
+    const posnARight = {
+      x: divA.offsetLeft + divA.offsetWidth + 8,
+      y: divA.offsetTop + divA.offsetHeight / 2
+    };
+    const posnBLeft = {
+      x: divB.offsetLeft - 8,
+      y: divB.offsetTop - 8
+    };
+    const posnBRight = {
+      x: divB.offsetLeft + divB.offsetWidth + 8,
+      y: divB.offsetTop + divA.offsetHeight / 2
+    };
+    const dStrLeft =
+      'M' +
+      (posnALeft.x + 55) + ',' + (posnALeft.y) + ' ' +
+      'C' +
+      (posnALeft.x + 55) + ',' + (posnALeft.y) + ' ' +
+      (posnBLeft.x + 55) + ',' + (posnBLeft.y) + ' ' +
+      (posnBLeft.x + 55) + ',' + (posnBLeft.y);
+    arrowLeft.setAttribute('d', dStrLeft);
     // const dStrRight =
     //   'M' +
     //   (posnBRight.x) + ',' + (posnBRight.y) + ' ' +
@@ -84,44 +119,45 @@ export class AppComponent implements OnInit {
 
 
 
-    const posnALeft = {
-      x: divA.offsetLeft - 8,
-      y: divA.offsetTop + 8
-    };
-    const posnARight = {
-      x: divA.offsetLeft + divA.offsetWidth + 8,
-      y: divA.offsetTop + divA.offsetHeight / 2
-    };
-    const posnBLeft = {
-      x: divB.offsetLeft - 8,
-      y: divB.offsetTop + divB.offsetHeight / 2
-    };
-    const posnBRight = {
-      x: divB.offsetLeft + divB.offsetWidth + 8,
-      y: divB.offsetTop + divA.offsetHeight / 2
-    };
-    const dStrLeft =
-      'M' +
-      (posnALeft.x ) + ',' + (posnALeft.y) + ' ' +
-      'C' +
-      (posnALeft.x - 100) + ',' + (posnALeft.y) + ' ' +
-      (posnBLeft.x - 100) + ',' + (posnBLeft.y) + ' ' +
-      (posnBLeft.x) + ',' + (posnBLeft.y);
-    arrowLeft.setAttribute('d', dStrLeft);
-    const dStrRight =
-      'M' +
-      (posnBRight.x) + ',' + (posnBRight.y) + ' ' +
-      'C' +
-      (posnBRight.x + 100) + ',' + (posnBRight.y) + ' ' +
-      (posnARight.x + 100) + ',' + (posnARight.y) + ' ' +
-      (posnARight.x) + ',' + (posnARight.y);
-    arrowRight.setAttribute('d', dStrRight);
+    // const posnALeft = {
+    //   x: divA.offsetLeft - 8,
+    //   y: divA.offsetTop + 8
+    // };
+    // const posnARight = {
+    //   x: divA.offsetLeft + divA.offsetWidth + 8,
+    //   y: divA.offsetTop + divA.offsetHeight / 2
+    // };
+    // const posnBLeft = {
+    //   x: divB.offsetLeft - 8,
+    //   y: divB.offsetTop + divB.offsetHeight / 2
+    // };
+    // const posnBRight = {
+    //   x: divB.offsetLeft + divB.offsetWidth + 8,
+    //   y: divB.offsetTop + divA.offsetHeight / 2
+    // };
+    // const dStrLeft =
+    //   'M' +
+    //   (posnALeft.x ) + ',' + (posnALeft.y) + ' ' +
+    //   'C' +
+    //   (posnALeft.x - 100) + ',' + (posnALeft.y) + ' ' +
+    //   (posnBLeft.x - 100) + ',' + (posnBLeft.y) + ' ' +
+    //   (posnBLeft.x) + ',' + (posnBLeft.y);
+    // arrowLeft.setAttribute('d', dStrLeft);
+    // const dStrRight =
+    //   'M' +
+    //   (posnBRight.x) + ',' + (posnBRight.y) + ' ' +
+    //   'C' +
+    //   (posnBRight.x + 100) + ',' + (posnBRight.y) + ' ' +
+    //   (posnARight.x + 100) + ',' + (posnARight.y) + ' ' +
+    //   (posnARight.x) + ',' + (posnARight.y);
+    // arrowRight.setAttribute('d', dStrRight);
 
   }
 
   public initDraw() {
     const that = this;
-    $('#a, #b').draggable({
+    console.log(this.listClass[0].concat(',').concat(this.listClass[1]));
+    $('#' + this.listClass[0].concat(',').concat('#' + this.listClass[1])).draggable({
       drag: function (event, ui) {
         that.draw();
       }
--- src/app/menu-left/menu-left.component.ts
+++ src/app/menu-left/menu-left.component.ts
@@ -12,21 +12,25 @@ export class MenuLeftComponent implements OnInit {
   public menuList = [
     {
       class: 'example-box aqua-gradient',
+      id: 'a',
       idInput: 'input1',
       idText: 'form1',
     },
     {
       class: 'example-box1 purple-gradient',
+      id: 'b',
       idInput: 'input2',
       idText: 'form2'
     },
     {
       class: 'example-box2 peach-gradient',
+      id: 'c',
       idInput: 'input3',
       idText: 'form3'
     },
     {
       class: 'example-box3 blue-gradient',
+      id: 'd',
       idInput: 'input4',
       idText: 'form4'
     },
@@ -37,7 +41,6 @@ export class MenuLeftComponent implements OnInit {
   }
 
   public drop(event: any) {
-    console.log(event);
    this.draw.emit(event);
   }
 
